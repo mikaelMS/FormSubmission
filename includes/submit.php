b9a907a2cbd324f8f4c780c7fb775e028f0ae031
Michael Schmidt <michaelschmidt@Michaels-MBP.fritz.box>
2019-06-02 13:54:19 +0200

    fetched data from form

--- includes/submit.php
+++ includes/submit.php
@@ -1,8 +1,21 @@
 <?php
-    include_once 'dbConfig.php';
+    include_once 'dbConfig.php';    
 
-    $sql = "INSERT INTO users (user_first, user_last, user_gender, user_newsletter) VALUES ('Diana', 'Bossaura', 'Female', 0);"; 
+    $first_name = $_POST['first'];
+    $last_name = $_POST['last'];
+    $gender = $_POST['gender'];
+    $newsletter = $_POST['newsletter'];
+
+    // Cause newsletter can be undefined => 0 or 1 BIT in DB
+    if ($newsletter != 1) {
+        $newsletter = 0; 
+    }
+
+    $sql = "INSERT INTO users (user_first, user_last, user_gender, user_newsletter) 
+            VALUES ('$first_name', '$last_name', '$gender', $newsletter);"; 
     mysqli_query($conn, $sql); 
 
     // After running the code above, it will load the following file
     header("Location: ../index.php?signup=success"); 
+
+    
